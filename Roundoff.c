73ea2f6b8c8f5bca70038c59451d675be4b2e810
Brundha M <brundhamohan11@gmail.com>
2024-12-17 16:17:57 +0530

    Create Roundoff.c

---
+++ Roundoff.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float inp;
+    scanf("%f",&inp);
+    printf("%d\n",(int)inp);
+    printf("%f\n",ceil(inp));
+    printf("%f\n",floor(inp));
+}
