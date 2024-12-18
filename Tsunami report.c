7a65714e4ba542e047d703b148ed560c5f4ce40a
Brundha M <brundhamohan11@gmail.com>
2024-12-18 16:32:40 +0530

    Create Tsunami report.c

---
+++ Tsunami report.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d\n%d\n%d",&a,&b,&c);
+    printf("1)Dead:%d\n",a);
+    scanf("%d",&a);
+    printf("2)Injured:%d\n",b);
+    scanf("%d",&b);
+    printf("3)Safe:%d\n",c);
+    scanf("%d",&c);
+    printf("Please help the people who are suffering!!!");
+}
