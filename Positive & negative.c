bed2484fc933f42b196ad8f36d41861f81d366b7
Brundha M <brundhamohan11@gmail.com>
2024-12-26 18:57:10 +0530

    Create Positive & negative.c

---
+++ Positive & negative.c
@@ -0,0 +1,15 @@
+Write a C program to find whether the given number is positive or negative.
+
+
+
+
+ #include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>=0)
+    printf("Positive");
+    else
+    printf("Negative");
+}
